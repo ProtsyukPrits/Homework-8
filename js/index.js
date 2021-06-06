c2f0b596865a3bda798950f5a216e335cd83b57f
Protsiuk-Prits Dmytro <protsiukprits95@gmail.com>
2021-06-06 19:35:05 +0200

    123

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 function sendEmail() {
-	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "<sender’s email address>",
-	Password : "<email password>",
-	To : 'protsiukprits95@gmail.com',
-	From : "<sender’s email address>",
-	Subject : "<email subject>",
-	Body : "wdwdwd",
-	}).then(
-		message => alert("mail sent successfully")
-	);
+
+	var name = $('#name').val();
+	var tel = $('#tel').val();
+	var email = $('#email').val();
+	var message = $('#message').val();
+	var form = 'name: ' + name + '<br>tel: ' + tel + '<br>email: ' + email + '<br>message: ' + message;
 }
+	Email.send({
+		Host: "smtp.gmail.com",
+		Username: "ivanovichdmytro@gmail.com",
+		Password: "",
+		To: 'dreamdream@gmail.com',
+		From: "protsiukprits95@gmail.com",
+		Subject: "New Mail On Contact Form From" + name,
+		Body: Body,
+	}).then(
+		message => {
+					
+			if (message == 'OK') {
+				alert('Your mail has been send. Thank you for connecting.');
+			}
+			else {
+				console.error(message);
+				alert('There is error at sending message. ')
+			}
+		}
+	);
